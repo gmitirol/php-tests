1a1096792f11094522681e929738422149e5e2a9
Andreas Erhard <andreas.erhard@i-med.ac.at>
2017-09-18 14:25:55 +0200

    Add test for the memcached extension.

---
+++ tests/memcached/MemcachedTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Gmi\PhpTests\Tests\memcached;
+
+use PHPUnit\Framework\TestCase;
+
+use Gmi\PhpTests\Tests\ExtensionChecker;
+use Memcached;
+
+class MemcachedTest extends TestCase
+{
+    private static $memcachedHost;
+    private static $memcachedPort;
+
+    public static function setUpBeforeClass()
+    {
+        self::$memcachedHost = getenv('MEMCACHED_HOST');
+        self::$memcachedPort = getenv('MEMCACHED_PORT');
+    }
+
+    public function setUp()
+    {
+        $checker = new ExtensionChecker(['memcached']);
+        if ($checker->check() === false) {
+            $this->markTestSkipped($checker->getMessage());
+        }
+
+        if (!self::$memcachedHost || !self::$memcachedPort) {
+            $this->markTestSkipped('Data for connecting to memcached is missing!');
+        }
+    }
+
+    /**
+     * @group php_memcached
+     * @group external_service
+     */
+    public function testMemcachedConnection()
+    {
+        $memcached = new Memcached();
+        $this->assertTrue($memcached->addServer(self::$memcachedHost, self::$memcachedPort));
+        $key = md5(uniqid('', true));
+        $date = date('c');
+        $this->assertTrue($memcached->set($key, $date));
+        $this->assertSame($date, $memcached->get($key));
+        $this->assertTrue($memcached->delete($key));
+    }
+}
